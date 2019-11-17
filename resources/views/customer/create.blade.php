070a4d7497db85c63f884c56e69d52c52f5cca3e
Ricardo Quintana <Programador1@infoguia.net>
2019-11-17 19:14:09 -0430

    Jquery validate modulo trabajador

--- resources/views/customer/create.blade.php
+++ resources/views/customer/create.blade.php
@@ -7,7 +7,7 @@
 @include('layouts.success')
 @include('layouts.errors')
 
-<form class="form" method="POST" action="{{ route('customer.store') }}"  role="form">
+<form id="customers" class="form" method="POST" action="{{ route('customer.store') }}"  role="form">
       @csrf
 		<div class="text-center">
 	    	<h3 class="title">Datos del Trabajador </h3>
@@ -27,15 +27,15 @@
 	  </div>
 	   <div class="form-group bmd-form-group">
 	    <label class="bmd-label-floating"><h7>Carnet</h7></label>
-	    <input type="number" name="carnet" class="form-control" id="number" placeholder="">
+	    <input type="number" name="carnet" class="form-control" id="carnet" placeholder="">
 	  </div>
 	   <div class="form-group bmd-form-group">
 	    <label class="bmd-label-floating"><h7>Telefono</h7></label>
-	    <input type="number" name="phone" class="form-control" id="phone" placeholder=""> 
+	    <input type="text" name="phone" class="form-control" id="phone">
 	  </div>
 	  <div class="form-group bmd-form-group">
 	    <label class="bmd-label-floating"><h7>Extensión</h7></label>
-	    <input type="number" name="extension" class="form-control" id="extension" placeholder=""> 
+	    <input type="number" name="extension" class="form-control" id="extension" maxlength="4" placeholder="">
 	  </div>
 		<div class="text-center">
 	    	<h3 class="title">Datos del Vehiculo </h3>
@@ -56,6 +56,101 @@
 	  <button type="submit" class="btn btn-primary">Guardar</button>
 	  <a href="{{route('customer.index')}}" class="btn btn-primary">Regresar</a>
 	</form>
+	<script>
+
+	if ($("#customers").length > 0) {
+		$("#phone").unmask().mask('(0999)9999999')
+		urlcarnet="{{url('/')}}/customer/carnet/"
+		urlplaca="{{url('/')}}/customer/placa/"
+	    $("#customers").validate({
+
+	    rules: {
+		    name: {
+		    	notNumber: true,
+		        required: true
+		    },
+		    last_name: {
+		    	notNumber: true,
+		        required: true
+		    },
+		    email: {
+	            required: true,
+	            emailvalid: true,
+		 	},
+		 	carnet: {
+	            required: true,
+	            carnetvalid: true,
+		 	},
+		 	phone: {
+	            required: true,
+	            minlength:13,
+		    	maxlength:13,
+	            phonevalid: true,
+		 	},
+		 	extension: {
+	            required: true,
+	            minlength:3,
+		    	maxlength:4,
+	            onlynumber: true,
+		 	},
+		 	model: {
+	            required: true,
+	            alphanumber: true,
+		 	},
+		 	placa: {
+	            required: true,
+	            placavalid: true,
+		 	},
+		 	color: {
+		    	notNumber: true,
+		        required: true
+		    },
+	    },
+	    messages: {
+		    name: {
+		        notNumber: "Por favor ingrese solo letras",
+		        required: "Por favor ingrese el Nombre",
+		    },
+		    last_name: {
+		        notNumber: "Por favor ingrese solo letras",
+		        required: "Por favor ingrese el Nombre",
+		    },
+	      	email: {
+		       required: "Por Favor Ingrese el Correo",
+		       emailvalid: "Ingrese un correo valido"
+	        },
+	        carnet: {
+		       required: "Por Favor Ingrese el Carnet",
+		       carnetvalid: "El Carnet ya existe"
+	        },
+	        phone: {
+			    required: "Por Favor Ingrese el Teléfono",
+			    minlength: "El Numero debe contener 11 Digitos",
+		    	maxlength: "El Numero debe contener 11 Digitos",
+			    phonevalid: "Ingrese un Teléfono con el Formato Valido"
+	        },
+	        extension: {
+			    required: "Por Favor Ingrese la Extensión",
+			    minlength: "El Numero debe contener entre 3 o 4  Digitos",
+		    	maxlength: "El Numero debe contener entre 3 o 4 Digitos",
+			    onlynumber: "Ingrese solo Números"
+	        },
+	        model: {
+	            required: "Por Favor Ingrese el Modelo",
+	            alphanumber: "Ingrese un Modelo Valido",
+		 	},
+		 	placa: {
+	            required: "Por Favor Ingrese la Placa",
+	            placavalid: "La Placa ya se Encuentra Registrada",
+		 	},
+		 	color: {
+		        notNumber: "Por favor ingrese solo letras",
+		        required: "Por favor ingrese el Color",
+		    },
+	    },
+	   });
+	}
+	</script>
 @endsection
 
 
